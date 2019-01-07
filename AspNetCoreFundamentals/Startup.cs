2e19f7e1152f707775384876453f7785879ad8e4
Christopher Jones <Chris@ITS-NHG-01>
2019-01-07 14:38:04 +0200

    remove: Greeter service. add: convention based default MVC routing.

--- AspNetCoreFundamentals/Startup.cs
+++ AspNetCoreFundamentals/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,13 +16,11 @@ namespace AspNetCoreFundamentals
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IGreeter, Greeter>();
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app,
                               IHostingEnvironment env,
-                              IGreeter greeter,
                               ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
@@ -31,14 +30,18 @@ namespace AspNetCoreFundamentals
 
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(ConfigureRoutes);
 
             app.Run(async (context) =>
             {
-                var greeting = greeter.GetMessageOfTheDay();
-
-                await context.Response.WriteAsync($"{greeting}: {env.EnvironmentName}");
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Not found");
             });
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
